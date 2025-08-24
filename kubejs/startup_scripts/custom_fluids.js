5495f9a11700072649e208093b7edc94fdd58479
Dibeo <simsimland2004@gmail.com>
2025-08-24 17:42:58 +0200

    add : custom certus crystal fluid

---
+++ kubejs/startup_scripts/custom_fluids.js
@@ -0,0 +1,6 @@
+StartupEvents.registry('fluid', event => {
+  event.create('liquid_certus_quartz')
+    .thickTexture(0x51799C)
+    .bucketColor(0x51799C)
+    .displayName('Liquid Certus Quartz')
+})

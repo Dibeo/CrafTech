816e30c909407ae7a5f9ea6bc5aecd6db150284b
Dibeo <simsimland2004@gmail.com>
2025-08-25 22:06:41 +0200

    update : tiab to use all gems

--- kubejs/server_scripts/hardification/timeinabottle.js
+++ kubejs/server_scripts/hardification/timeinabottle.js
@@ -3,9 +3,14 @@ ServerEvents.recipes((event) => {
     event.remove({output : "tiab:time_in_a_bottle"})
   event.recipes.industrialforegoing.dissolution_chamber(
     [
-      "#forge:gems/coin",
+      "#forge:plates/gold",
       "#forge:gems/dimensional",
+      "#forge:plates/gold",
+      "#forge:gems/coin",
       "#forge:gems/nether",
+      "ae2:quartz_vibrant_glass",
+      "#forge:gems/compressed_emerald",
+      "ae2:quartz_vibrant_glass",
     ], // input items
     "minecraft:milk", // input fluid
     "tiab:time_in_a_bottle", // output item

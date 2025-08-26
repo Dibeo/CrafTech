a6a4e2d4f294902afe219e852251c3541988750b
Dibeo <simsimland2004@gmail.com>
2025-08-26 16:21:48 +0200

    add : custom craft for the unit that now need the previous size

--- kubejs/server_scripts/hardification/indusforegoing.js
+++ kubejs/server_scripts/hardification/indusforegoing.js
@@ -21,7 +21,7 @@ ServerEvents.recipes((event) => {
     event.remove([{ id: `industrialforegoing:${x}_gear` }]);
   });
 
-    event.replaceInput(
+  event.replaceInput(
     { mod: "industrialforegoing" },
     "#forge:cobblestone",
     "#pneumaticcraft:compressed_stone"
@@ -32,4 +32,32 @@ ServerEvents.recipes((event) => {
     "#forge:dusts/redstone",
     "pneumaticcraft:transistor"
   );
+
+  let tank_unit = [
+    "industrialforegoing:common_black_hole_tank",
+    "industrialforegoing:pity_black_hole_tank",
+    "industrialforegoing:simple_black_hole_tank",
+    "industrialforegoing:advanced_black_hole_tank",
+    "industrialforegoing:supreme_black_hole_tank",
+  ];
+  for (const i = 1; i < tank_unit.length; ++i)
+    event.replaceInput(
+      { mod: "industrialforegoing", output: tank_unit[i] },
+      "#forge:ender_pearls",
+      tank_unit[i - 1]
+    );
+
+    let item_unit = [
+    "industrialforegoing:common_black_hole_unit",
+    "industrialforegoing:pity_black_hole_unit",
+    "industrialforegoing:simple_black_hole_unit",
+    "industrialforegoing:advanced_black_hole_unit",
+    "industrialforegoing:supreme_black_hole_unit",
+  ];
+  for (const i = 1; i < item_unit.length; ++i)
+    event.replaceInput(
+      { mod: "industrialforegoing", output: item_unit[i] },
+      "#forge:ender_pearls",
+      item_unit[i - 1]
+    );
 });
